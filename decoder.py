ddf993ea90918f88f82865dce03095d19ad17fb7
king-sawyer <kingsawyer555@gmail.com>
2026-04-27 18:42:13 -0700

    Initial decoder boilerplate info

---
+++ decoder.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.io import wavfile
+
+SAMPLE_RATE = 48000
+BAUD = 300
+
+FREQ_0 = 2025
+FREQ_1 = 2225
+
+def tone_power(samples, N, f, fs):
+    return
+
+def main():
+    print("Decoder!")
+
+if __name__ == "__main__":
+    main()
